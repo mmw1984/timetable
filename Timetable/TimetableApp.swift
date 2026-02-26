b168d0905414b334364e4f7054831f88942360c2
mmw1984 <me@mmw1984.com>
2026-02-26 21:42:50 +0800

    Initial Commit

---
+++ Timetable/TimetableApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimetableApp.swift
+//  Timetable
+//
+//  Created by Marco on 26/2/2026.
+//
+
+import SwiftUI
+
+@main
+struct TimetableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
